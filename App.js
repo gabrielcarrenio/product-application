6fc0bbb5154c5bc240daff6fb2e2417d51c7b98a
GabrielCarreño <carrenocarlosgabriel@gmail.com>
2020-12-27 20:35:41 -0300

    finished

--- App.js
+++ App.js
@@ -17,6 +17,7 @@ class UI {
                     <strong>Product Name</strong>: ${product.name}
                     <strong>Product Price</strong>: ${product.price}
                     <strong>Product Year</strong>: ${product.year}
+                    <a href="#" class="btn btn-danger" name="delete">Delete</a>
                 </div>
             </div>
         `;
@@ -28,12 +29,24 @@ class UI {
         document.getElementById('product-form').reset();
     }
 
-    deleteProduct() {
-
+    deleteProduct(element) {
+        if(element.name === 'delete') {
+            element.parentElement.parentElement.parentElement.remove();
+            this.showMessage('Product Delet Successfuly', 'info');
+        }
     }
 
-    showMessage( ) {
-
+    showMessage( message, cssClass) {
+        const div = document.createElement('div');
+        div.className = `alert alert-${cssClass} mt-1`;
+        div.appendChild(document.createTextNode(message));
+        //Showing in Dom
+        const container = document.querySelector('.container');
+        const app = document.querySelector('#App');
+        container.insertBefore(div, app);
+        setTimeout(function () {
+            document.querySelector('.alert').remove();
+        }, 3000);
     }
 }
 
@@ -46,8 +59,18 @@ document.getElementById('product-form')
 
         const product = new Product(name, price, year);
         const ui = new UI();
+
+        if(name === '' || price === '' || year === '') {
+            return ui.showMessage('Complete fields Please', 'danger');
+        }
         ui.addProduct(product);
         ui.resetForm();
+        ui.showMessage('Product Added Successfully', 'success');
 
         e.preventDefault();
     });
+
+document.getElementById('product-list').addEventListener('click', function(e) {
+    const ui = new UI();
+    ui.deleteProduct(e.target)
+});
